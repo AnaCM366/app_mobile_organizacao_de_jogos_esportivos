6c6424f22495e55817588a4021c8f41b216fcf4f
AnaCM366 <anacarolinamachado366@gmail.com>
2026-02-19 21:56:28 -0300

    Criação da tela de login

---
+++ lib/pages/cadastroUsuario_form_page.dart
@@ -0,0 +1,47 @@
+// import 'package:flutter/material.dart';
+
+// class CadastroFormPage extends StatefulWidget {
+//   final String? id;
+//   const CadastroFormPage({super.key, this.id});
+
+//   @override
+//   State<CadastroFormPage> createState() => _CadastroFormPageState();
+// }
+
+// class _CadastroFormPageState extends State<CadastroFormPage> {
+//   late TextEditingController controllerName;
+//   late TextEditingController controllerSenha;
+
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: .center,
+//           children: [
+//             const Text('You have pushed the button this many times:'),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headlineMedium,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
